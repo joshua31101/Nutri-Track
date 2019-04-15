37b19b52e3ccc8365b53e0eb57fe8c7fc7d38f23
Joshua Wang <joshuawang3110@gmail.com>
2019-04-15 18:07:49 -0400

    Display join now and my food button based on login status in home page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('isAuthenticated', this.get('session.isAuthenticated'));
+  }
 });
